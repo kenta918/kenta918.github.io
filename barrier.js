ba6d1d3da311d8adbc8547a99b4f848f368f10eb
kenta918 <56744380+kenta918@users.noreply.github.com>
2020-03-13 13:40:33 -1000

    Add files via upload

---
+++ barrier.js
@@ -0,0 +1,33 @@
+
+
+function barrier(){
+  ranY = Math.floor(Math.random() * h);
+  this.x2= w-20;
+  this.y2= ranY;
+  this.y3 = random()
+  this.gravity2= random(0.5,1);
+  this.velocity2 = random(0,2);
+  
+  this.ret = function(){
+    fill(random(0,255),random(0,255),random(0,255))
+    
+    rect(this.x2,this.y2,200,20)
+    texture(mario);
+  }
+  this.update2 = function(){
+    this.velocity2 += this.gravity2;
+    this.x2 -= this.velocity2;
+    ranY2 = Math.floor(Math.random() * h);
+
+    this.velocity2 *= 0.9;
+    if((this.x2 < 0) || (this.y2 +100 >h)){
+      this.y2 = ranY2;
+      this.x2= w-20;
+      document.getElementById("score").innerHTML ++;
+      
+    }
+    
+    
+  }
+}
+
---
+++ jumper.js
@@ -0,0 +1,48 @@
+
+
+function jumper(){
+  this.x=w/2;
+  this.y=0;
+  this.gravity= 0.5;//for force of gravity
+  this.lift = -15;//the opposing force of gravity when I jump
+  this.velocity = 0; //speed of gravity force
+  
+  //I am building  function to display on the screen this is where I put my values of wht the jumper will look like
+  this.show = function(){
+    fill(255,0,0);
+    ellipse(this.x,this.y,50,50);
+    
+  }
+  
+  //build a function called up that will take the intial velocity and modify by opposing gravity
+  this.up = function(){
+    //if(jumpcount < 2){
+    this.velocity += this.lift;
+
+    //}
+    
+    
+  }
+  //this will continuously update the jumper
+  this.update = function(){
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity *= 0.9;//air resistance
+    //this will prevent the jumoer from leaving the ground
+    if(this.y > h){
+      this.y=h;
+      this.velocity=0;
+      //jumpcount = 0;
+      location.reload();
+      window.alert("game over");
+      
+    }
+    //this will prevent the jumper from leaving the ceiling
+    if(this.y < 0){
+      this.y =0;
+      this.velocity = 0;
+    }
+
+  }
+  
+}
