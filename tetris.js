98b6f07443e694640c2f142caefbe263a2e6b5d5
kenta918 <56744380+kenta918@users.noreply.github.com>
2020-05-24 00:08:49 -1000

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -1,29 +1,73 @@
 
 
 function cube(){
-  //this.axis = [[343],[377.5],[412],[446.5],[481],[515.5],[550],[584.5],[619],[653.5],[688],[722.5],[757],[791.5],[826],[860.5],[895],[929.5],[964],[998.5],[1033]]
-  this.x= random(343,1033);
-  this.y= 20;
+  //this.axis = [axis]
+  this.x= random(-400,290);
+  this.y=-300;
   this.w=69;
   this.h=69;
-  this.speed3=speed1;
-  this.speed4=speed2;
+  this.speed1=1;
+  this.speed2=4;
   this.update  = function(){
-    
-    if(this.y < 640){
-      this.y += random(this.speed3,this.speed4);
-    }
-    else if((this.y ==  640) ||(this.y > 640  )){
+    this.y += random(this.speed1,this.speed2);
+    if(this.y > 241){
+      this.x =(random(-400,290));
+      this.y =-300;
       document.getElementById("health").innerHTML --;
-      
-      this.x =(343,1033);
-      this.y =20
-      
     }
+    if(document.getElementById("health").innerHTML == 0){
+       location.reload();
+        window.alert("you died");
+      storeItem('sco',document.getElementById("score").innerHTML);
+      storeItem('lv',document.getElementById("lvl").innerHTML);
+      //highScore = getItem('sco');
+      document.getElementById("past").innerHTML = getItem('sco')
+      document.getElementById("past").innerHMTL = getitem('lv')
+      window.alert()
+}
+
+    if(document.getElementById("score").innerHTML== 20){
+        document.getElementById("lvl").innerHTML=2;
+        this.speed1 = 2;
+        this.speed2=5;
+        moveRight = 10
+        moveLeft =10;
+        shooter.lift = -70
+ }
+    else if(document.getElementById("score").innerHTML == 40){
+        document.getElementById("lvl").innerHTML =3;
+  
+        this.speed1 =2;
+        this.speed2=6;
+        moveRight = 11
+        moveLeft =11;
+        shooter.lift = -80
+    }
+    else if(document.getElementById("score").innerHTML ==60){
+        document.getElementById("lvl").innerHTML= 4;
+  
+        this.speed1 =2;
+        this.speed2=7;
+        moveRight = 12
+        moveLeft =12
+        shooter.lift = -90
+      }
+    else if(document.getElementById("score").innerHTML ==80){
+      document.getElementById("lvl").innerHTML =5
+ 
+      this.speed1 =2;
+      this.speed2=8;
+      moveRight = 13
+      moveLeft =13
+      shooter.lift = -200
+  
+    }
+    
   }
   this.show = function(){
     
     fill(255,0,0);
+    texture(img2)
     rect(this.x,this.y,this.w,this.h)
     
     
@@ -34,16 +78,16 @@ function cube(){
 }
 
 function shooter(){
-  this.x=722;
-  this.y=675;
-  this.x2=770;
-  this.y2 =710;
-  this.x3= 673;
-  this.y3=710
-  this.bullet =720
+  this.x=0;
+  this.y=250;
+  this.x2=50;
+  this.y2 =320;
+  this.x3= -50;
+  this.y3=320
+  this.bullet =700
   this.xc = this.x
   this.gravity = 0
-  this.lift=-30
+  this.lift=-60
   this.velo=0
   
   this.update2 = function(){
@@ -51,7 +95,7 @@ function shooter(){
   }
   this.show2 = function(){
     fill(255,0,0)
-    
+    texture(img)
     triangle(this.x,this.y,this.x2,this.y2,this.x3,this.y3)
   }
   
@@ -69,11 +113,11 @@ function shooter(){
   this.update3 = function(){
     this.velo += this.gravity;
     this.bullet += this.velo;
-    if(this.bullet>720){
-      this.bullet = 720
+    if(this.bullet>320){
+      this.bullet = 320
     }
-    if(this.bullet <20){
-      this.bullet = 720;
+    if(this.bullet <-300){
+      this.bullet = 320;
       this.velo = 0
       this.xc =this.x
     }
