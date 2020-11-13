855e80342cf44e6c0769192a6e3463a75129c567
kenta918 <56744380+kenta918@users.noreply.github.com>
2020-11-12 19:32:07 -1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -42,7 +42,7 @@ function drawKeypoints()  {
       let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
+        fill(50, 168, 70);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
@@ -59,7 +59,7 @@ function drawSkeleton() {
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
-      stroke(255, 0, 0);
+      stroke(50, 168, 70);
       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
     }
   }
